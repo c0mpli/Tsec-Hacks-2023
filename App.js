4bb6269a13baca9472ef700fc364ea20fc89fd78
VISHESH  GATHA <101997238+vishesh9096@users.noreply.github.com>
2023-02-03 04:55:58 +0530

    cardsss

--- App.js
+++ App.js
@@ -36,33 +36,31 @@ export default function App() {
     <NavigationContainer>
     <TailwindProvider>
     <Stack.Navigator>
-<<<<<<< Updated upstream
 
-=======
     <Stack.Screen name="Home" component={HomeScreen} />
     <Stack.Screen name="Job2" component={Job2Screen} />
     <Stack.Screen name="Job" component={JobScreen} />
     <Stack.Screen name="Fy" component={FyScreen} />
     
-    <Stack.Screen name="Ani2"  component={Ani2Screen} />
->>>>>>> Stashed changes
+
+
     
 
 
-<<<<<<< Updated upstream
+
 
     <Stack.Screen name="AllJ" component={AllJobs} />
     <Stack.Screen name="Initital" component={InitialScreen} />
-    <Stack.Screen name="Job" component={JobScreen} />
+
     <Stack.Screen name="Ani2"  component={Ani2Screen} />
     <Stack.Screen name="Commu" component={Community} />
-    <Stack.Screen name="Home" component={HomeScreen} />
-=======
+
+
     <Stack.Screen name="Profile" component={ProfileScreen} />
     <Stack.Screen name="Initial" component={InitialScreen} />
     <Stack.Screen name="set" component={SettingScreen} />
     
->>>>>>> Stashed changes
+
     <Stack.Screen name="Data1" component={Data1Screen} />
     <Stack.Screen name="Dash" component={Dashboard} />
     
