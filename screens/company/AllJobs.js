8e584651c456993f3dfc9d1ba284f387ec7ce785
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2023-02-03 07:15:47 +0530

    headers updated

--- screens/company/AllJobs.js
+++ screens/company/AllJobs.js
@@ -12,9 +12,14 @@ const AllJobs = () => {
   const [jobs,setJobs] = useState()
   let headers = {
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers':
+        'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST',
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': '*',
+      'X-Requested-With': '*',
+    },
 
 }
     async function fetchData(){
