15efa7199a55ca2326dd521022f2344839ccb12d
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2023-02-03 05:48:22 +0530

    test

--- App.js
+++ App.js
@@ -37,6 +37,7 @@ export default function App() {
     <TailwindProvider>
     <Stack.Navigator>
 
+    <Stack.Screen name="AllJ" component={AllJobs} />
     <Stack.Screen name="Home" component={HomeScreen} />
     <Stack.Screen name="Job2" component={Job2Screen} />
     <Stack.Screen name="Job" component={JobScreen} />
@@ -49,7 +50,6 @@ export default function App() {
 
 
 
-    <Stack.Screen name="AllJ" component={AllJobs} />
     <Stack.Screen name="Initital" component={InitialScreen} />
 
     <Stack.Screen name="Ani2"  component={Ani2Screen} />
